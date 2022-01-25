775b1460b1d0e4a8968f0c1f83d58a8bac0e09a6
C1324634 Ricardo Carteri Uehlein <C1324634@bb.com.br>
2022-01-25 16:20:51 -0300

    feat: pokemon service loading data from api

---
+++ src/_services/pokemon.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from, map, merge, mergeMap, ReplaySubject } from 'rxjs';
+import { Pokemon } from 'src/_model/Pokemon';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  public pokemons: Pokemon[] = [];
+
+  constructor(private httpClient: HttpClient,) {
+    const allPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000000'
+    this.httpClient.get<any>(allPokemonsUrl).pipe(
+      map(value => value.results),
+      map((value: any) => {
+        return from(value).pipe(
+          mergeMap((v: any) => this.httpClient.get(v.url))
+        );
+      }),
+      mergeMap(value => value),
+    ).subscribe((result: any) => {
+      const pokemon: Pokemon = {
+        image: result.sprites.front_default,
+        number: result.id,
+        name: result.name,
+        types: result.types.map(t => t.type.name),
+      };
+      this.pokemons[result.id] = pokemon;
+    });
+  }
+}
--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Pokemon } from '../../_model/Pokemon';
+import { getPokemonImage, getPokemonNumber, Pokemon } from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -9,14 +9,6 @@ import { Pokemon } from '../../_model/Pokemon';
 export class PokemonCardComponent {
   @Input()
   public pokemon: Pokemon;
-
-  public leadingZero(str: string | number, size = 3): string {
-    let s = String(str);
-
-    while (s.length < (size || 2)) {
-      s = '0' + s;
-    }
-
-    return s;
-  }
+  public getPokemonNumber = getPokemonNumber;
+  public getPokemonImage = getPokemonImage;
 }
