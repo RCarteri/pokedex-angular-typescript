6d528973e3cc5f2890d8a996ffa8c3e25b485e03
C1324634 Ricardo Carteri Uehlein <C1324634@bb.com.br>
2022-01-26 11:11:04 -0300

    upload arquivos

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { getPokemonImage, getPokemonNumber, Pokemon } from '../../_model/Pokemon';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.sass'],
+})
+export class PokemonCardComponent {
+  @Input()
+  public pokemon: Pokemon;
+  public getPokemonNumber = getPokemonNumber;
+  public getPokemonImage = getPokemonImage;
+}
